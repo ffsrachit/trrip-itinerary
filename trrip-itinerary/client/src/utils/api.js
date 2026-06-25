23207d993d0e97923f19e4bcb3f68bc0e5cf3e0c
Rachit Verma <rachit28082004@gmail.com>
2026-06-25 11:29:13 +0530

    fix: update API base URL to correct backend endpoint

--- trrip-itinerary/client/src/utils/api.js
+++ trrip-itinerary/client/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://trrip-itinerary.onrender.com'
+  baseURL: 'https://trrip-backend-1-4jca.onrender.com'
 });
 
 API.interceptors.request.use((req) => {
